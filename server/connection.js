35f6dda0f0203707024d576f0f3fdfd76ade7317
Yuvraj <92994932+i-am-yuvi@users.noreply.github.com>
2024-09-01 13:57:23 +0530

    Update connection.js

--- server/connection.js
+++ server/connection.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const connect=async()=>{
     try {
-        await mongoose.connect("mongodb+srv://yuvraj:MNWWxAQhngKpPrix@gymkhana.uxvzkwk.mongodb.net/?retryWrites=true&w=majority&appName=gymkhana",{ useNewUrlParser: true });
+        await mongoose.connect("mongodb+srv://yuvraj:makeyourown@gymkhana.uxvzkwk.mongodb.net/?retryWrites=true&w=majority&appName=gymkhana",{ useNewUrlParser: true });
         console.log("database connected");
     } catch (error) {
         console.log(error);
